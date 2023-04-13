16613442f4aceb42eb1f6d712a11b2d76c8abdf0
Pubudu Janith <pubudujanith123@gmail.com>
2023-04-13 18:43:55 +0530

    Complete UserRepository logic

--- src/main/java/lk/pubudu/app/repository/UserRepository.java
+++ src/main/java/lk/pubudu/app/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package lk.pubudu.app.repository;
 
+import lk.pubudu.app.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, String> {
 }
