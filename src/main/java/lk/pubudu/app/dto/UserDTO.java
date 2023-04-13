b1a9ef859e1af805640d578fb9aea75fc5931f18
Pubudu Janith <pubudujanith123@gmail.com>
2023-04-13 18:47:51 +0530

    Complete UserDTO logic

---
+++ src/main/java/lk/pubudu/app/dto/UserDTO.java
@@ -0,0 +1,28 @@
+package lk.pubudu.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO implements Serializable {
+    @NotBlank(message = "User email cannot be empty or null")
+    @Email(message = "Invalid user email address")
+    private String email;
+    @NotBlank(message = "User name cannot be empty or null")
+    @Pattern(regexp = "^[A-Za-z][A-Za-z ]+$", message = "Invalid user name")
+    private String name;
+    @NotBlank(message = "User address cannot be empty or null")
+    @Pattern(regexp = "^[A-Za-z\\d][A-Za-z\\d-|/# ,.:;\\\\]+$", message = "Invalid user address")
+    private String address;
+    @NotBlank(message = "User contact number cannot be empty or null")
+    @Pattern(regexp = "^\\d{3}-\\d{7}$", message = "Invalid user contact number")
+    private String contact;
+}
