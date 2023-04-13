23b9527cd78b6ac17b31f5a545cf9fc237e3c468
Pubudu Janith <pubudujanith123@gmail.com>
2023-04-13 18:41:59 +0530

    Complete User entity logic

---
+++ src/main/java/lk/pubudu/app/entity/User.java
@@ -0,0 +1,25 @@
+package lk.pubudu.app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User implements SuperEntity {
+    @Id
+    @Column(columnDefinition = "VARCHAR(100)")
+    private String email;
+    @Column(nullable = false, columnDefinition = "VARCHAR(100)")
+    private String name;
+    @Column(nullable = false, columnDefinition = "VARCHAR(150)")
+    private String address;
+    @Column(nullable = false, columnDefinition = "VARCHAR(11)")
+    private String contact;
+}
---
+++ src/main/java/lk/pubudu/app/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package lk.pubudu.app.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+}
