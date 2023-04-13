d2068466a82179b20549afe05c02e93b06d9feb9
Pubudu Janith <pubudujanith123@gmail.com>
2023-04-13 20:24:12 +0530

    Complete email sending logic in service layer

--- src/main/java/lk/pubudu/app/service/custom/impl/UserServiceImpl.java
+++ src/main/java/lk/pubudu/app/service/custom/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import lk.pubudu.app.entity.User;
 import lk.pubudu.app.repository.UserRepository;
 import lk.pubudu.app.service.custom.UserService;
 import lk.pubudu.app.service.util.Transformer;
+import lk.pubudu.app.util.EMailSender;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
@@ -17,11 +18,13 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final Transformer transformer;
+    private final EMailSender eMailSender;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, Transformer transformer) {
+    public UserServiceImpl(UserRepository userRepository, Transformer transformer, EMailSender mailSender) {
         this.userRepository = userRepository;
         this.transformer = transformer;
+        this.eMailSender = mailSender;
     }
 
     @Override
@@ -30,5 +33,6 @@ public class UserServiceImpl implements UserService {
         Optional<User> user = userRepository.findById(userDTO.getEmail());
         if (user.isPresent()) throw new DuplicateKeyException("A user is already exists with this email address");
         userRepository.save(transformer.toUserEntity(userDTO));
+        eMailSender.sendMail(userDTO.getEmail(), "Confirmation", "User: ".concat(userDTO.getName()).concat(" has successfully saved to the database"));
     }
 }
